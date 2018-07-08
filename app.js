06365312d952c9d191beecd642cab3aaaba67a75
Rahul Pargi <rahulpargi@gmail.com>
2018-07-08 11:05:31 -0500

    UI Updates

--- app.js
+++ app.js
@@ -46,8 +46,15 @@ app.get('/',function(req,res){
             });
         }   
     });
-    
-    
+});
+
+//Getting single Article
+app.get('/article/:id',function(req,res){
+    Article.findById(req.params.id,function(err,article){
+        res.render('article',{
+            article:article
+        });
+    });
 });
 
 //Add Route
