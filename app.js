10b6371fdfbe740c4c574d95c25f0d0410f5aa6a
Rahul Pargi <rahulpargi@gmail.com>
2018-07-07 21:26:40 -0500

    Fetching Data from Database

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ db.on('error',function(error){
 //init app
 const app=express();
 
+//Fetching Models
+const Article=require('./models/article');
+
 //Load View Engine
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','pug');
@@ -24,27 +27,18 @@ app.set('view engine','pug');
 
 //Home Route
 app.get('/',function(req,res){
-    let articles=[
-        {
-            id:1,
-            title:'Article One',
-            body:'This is article one'
-        },
-        {
-            id:2,
-            title:'Article Two',
-            body:'This is article Two'
-        },
-        {
-            id:3,
-            title:'Article Three',
-            body:'This is article Three'
-        },
-    ];
-    res.render('index',{
-        title:'Articles',
-        articles:articles
+    Article.find({},function(err,articles){
+        if(err){
+            console.log(err);
+        }else{
+            res.render('index',{
+                title:'Articles',
+                articles:articles
+            });
+        }   
     });
+    
+    
 });
 
 //Add Route
