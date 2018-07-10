42d1c61a657e00294b499d8412bcf23b5c4cd35b
Rahul Pargi <rahulpargi@gmail.com>
2018-07-09 21:45:36 -0500

    Update and Delete Articles

--- app.js
+++ app.js
@@ -74,6 +74,7 @@ app.post('/articles/edit/:id',function(req,res){
     article.author=req.body.author;
     article.body=req.body.body;
     let query={_id:req.params.id};
+    
     Article.update(query,article,function(err){
         if(err){
             console.log(err);
@@ -81,6 +82,16 @@ app.post('/articles/edit/:id',function(req,res){
         }else{
             res.redirect('/');
         }
+    });
+});
+
+app.delete('/article/:id',function(req,res){
+    let query={_id:req.params.id}
+    Article.remove(query,function(err){
+        if(err){
+            console.log(err);
+        }
+        res.send('Success');
     });
 });
 
